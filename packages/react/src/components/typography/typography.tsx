3c2af93efddff2d7cba4e481ec7ea0d180496f1f
Kha Nguyen <hi@khanguyen.com>
2025-06-15 00:42:33 -0500

    fix(Typography): fix issue where default size and weight override variant

--- packages/react/src/components/typography/typography.tsx
+++ packages/react/src/components/typography/typography.tsx
@@ -3,15 +3,17 @@ import { cva } from "class-variance-authority"
 
 import { cx } from "../../lib"
 
-const typographyVariants = cva("", {
+const typographyVariants = cva("tracking-tight", {
   variants: {
     variant: {
-      h1: "scroll-m-20 text-4xl font-extrabold tracking-tight",
-      h2: "scroll-m-20 text-3xl font-semibold tracking-tight",
-      h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
-      h4: "scroll-m-20 text-xl font-semibold tracking-tight",
-      p: "leading-7 [&:not(:first-child)]:mt-6",
-      blockquote: "mt-6 border-l-2 border-foreground/20 pl-6 italic",
+      h1: "scroll-m-20 text-5xl font-extrabold mb-6",
+      h2: "scroll-m-20 text-4xl font-semibold mb-4",
+      h3: "scroll-m-20 text-3xl font-semibold mb-3",
+      h4: "scroll-m-20 text-2xl font-semibold mb-2",
+      h5: "scroll-m-20 text-xl font-semibold mb-2",
+      h6: "scroll-m-20 text-lg font-semibold mb-1",
+      p: "leading-7",
+      blockquote: "my-6 border-l-2 border-foreground/20 pl-6 italic",
       list: "my-6 ml-6 list-disc [&>li]:mt-2",
       lead: "text-xl text-foreground/80",
       muted: "text-sm text-foreground/60",
@@ -19,9 +21,12 @@ const typographyVariants = cva("", {
     fontSize: {
       xs: "text-xs",
       sm: "text-sm",
-      md: "",
+      md: "text-base",
       lg: "text-lg",
       xl: "text-xl",
+      "2xl": "text-2xl",
+      "3xl": "text-3xl",
+      "4xl": "text-4xl",
     },
     fontWeight: {
       thin: "font-thin",
@@ -34,11 +39,68 @@ const typographyVariants = cva("", {
       extrabold: "font-extrabold",
     },
   },
-  defaultVariants: {
-    variant: "p",
-    fontSize: "md",
-    fontWeight: "normal",
-  },
+  compoundVariants: [
+    {
+      fontWeight: "normal",
+      variant: "h1",
+      class: "font-extrabold",
+    },
+    {
+      fontWeight: "normal",
+      variant: "h2",
+      class: "font-extrabold",
+    },
+    {
+      fontWeight: "normal",
+      variant: "h3",
+      class: "font-semibold",
+    },
+    {
+      fontWeight: "normal",
+      variant: "h4",
+      class: "font-semibold",
+    },
+    {
+      fontWeight: "normal",
+      variant: "h5",
+      class: "font-semibold",
+    },
+    {
+      fontWeight: "normal",
+      variant: "h6",
+      class: "font-semibold",
+    },
+    {
+      fontSize: "md",
+      variant: "h1",
+      class: "text-5xl",
+    },
+    {
+      fontSize: "md",
+      variant: "h2",
+      class: "text-4xl",
+    },
+    {
+      fontSize: "md",
+      variant: "h3",
+      class: "text-3xl",
+    },
+    {
+      fontSize: "md",
+      variant: "h4",
+      class: "text-2xl",
+    },
+    {
+      fontSize: "md",
+      variant: "h5",
+      class: "text-xl",
+    },
+    {
+      fontSize: "md",
+      variant: "h6",
+      class: "text-lg",
+    },
+  ],
 })
 
 type TypographyElement =
@@ -60,12 +122,14 @@ interface TypographyProps extends React.HTMLAttributes<HTMLElement> {
     | "h2"
     | "h3"
     | "h4"
+    | "h5"
+    | "h6"
     | "p"
     | "blockquote"
     | "list"
     | "lead"
     | "muted"
-  fontSize?: "xs" | "sm" | "md" | "lg" | "xl"
+  fontSize?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl"
   fontWeight?:
     | "thin"
     | "extralight"
